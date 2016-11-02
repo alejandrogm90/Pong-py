a007c6ab08531074e6c37d2f2fd534c89c5ec1e9
Alejandro Gomez <alejandrogomezmartin90@gmail.com>
2016-11-02 14:34:06 +0100

    Update INSTALL.sh

--- INSTALL.sh
+++ INSTALL.sh
@@ -2,10 +2,18 @@
 
 dir1='/opt/Pong-py'
 
-echo 'Copying all files.'
-mkdir $dir1
-cp -R images $dir1
-cp main.py $dir1
-echo 'Copy done'
+function error1() {
+  echo 'There was a problem during installation.'
+  exit $1
+}
 
-ln -s $dir1'/main.py' ~/Pong-py
+echo 'Copying all files.'
+if [ `mkdir $dir1` ] ; then
+  cp -R images $dir1
+  cp main.py $dir1
+  echo 'Copy done.'
+  
+  ln -s $dir1'/main.py' ~/Pong-py
+else
+  error 1
+fi
